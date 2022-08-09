96548c7aa01dea97cd36a1acd00f7bd12646c087
Mick McGrath <mick@bitovi.com>
2022-08-09 11:56:50 -0400

    remove additional output from fetch script

--- scripts/catalog/fetch.sh
+++ scripts/catalog/fetch.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
-echo "Fetching the catalog"
 
 cd $BUILD_CONTENTS_DIRECTORY
-echo "Running 'npm run fetch-catalog'"
-npm run fetch-catalog
+NODE_OPTIONS="-r dotenv/config -r cross-fetch/polyfill" $BUILD_CONTENTS_DIRECTORY/node_modules/.bin/ts-node ./scripts/catalog/fetch-contentful.ts
 cd -
