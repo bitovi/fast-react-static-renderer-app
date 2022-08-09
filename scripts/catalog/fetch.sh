fb1a7f9859949666278f6411ecbcc804f12a4c1f
Mick McGrath <mick@bitovi.com>
2022-08-09 09:56:49 -0400

    use npm command to fetch catalog

--- scripts/catalog/fetch.sh
+++ scripts/catalog/fetch.sh
@@ -3,5 +3,6 @@ set -e
 echo "Fetching the catalog"
 
 cd $BUILD_CONTENTS_DIRECTORY
-NODE_OPTIONS="-r dotenv/config -r cross-fetch/polyfill" $BUILD_CONTENTS_DIRECTORY/node_modules/.bin/ts-node ./scripts/catalog/fetch-contentful.ts
+echo "Running 'npm run fetch-catalog'"
+npm run fetch-catalog
 cd -
