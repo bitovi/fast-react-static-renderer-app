beef55bd09c9ed0bda636c3dcdb014ec75101628
Mick McGrath <mick@bitovi.com>
2022-08-09 09:48:01 -0400

    try running ts-node from within build contents dir

--- scripts/catalog/fetch.sh
+++ scripts/catalog/fetch.sh
@@ -2,4 +2,6 @@
 set -e
 echo "Fetching the catalog"
 
+cd $BUILD_CONTENTS_DIRECTORY
 NODE_OPTIONS="-r dotenv/config -r cross-fetch/polyfill" $BUILD_CONTENTS_DIRECTORY/node_modules/.bin/ts-node ./scripts/catalog/fetch-contentful.ts
+cd -
