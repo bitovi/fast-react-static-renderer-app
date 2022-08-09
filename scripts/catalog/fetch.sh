e092d0b80f9355b52ac2833f8f31994c2ace0687
Mick McGrath <mick@bitovi.com>
2022-08-09 12:12:28 -0400

    do not cd back to previous directory in fetch script

--- scripts/catalog/fetch.sh
+++ scripts/catalog/fetch.sh
@@ -3,4 +3,3 @@ set -e
 
 cd $BUILD_CONTENTS_DIRECTORY
 NODE_OPTIONS="-r dotenv/config -r cross-fetch/polyfill" $BUILD_CONTENTS_DIRECTORY/node_modules/.bin/ts-node ./scripts/catalog/fetch-contentful.ts
-cd -
