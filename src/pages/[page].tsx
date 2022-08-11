0f5918d5834d792e5c2e8045d0858e3e33572f55
David Nicholas <34694793+DavidNic11@users.noreply.github.com>
2022-08-11 14:01:40 -0700

    Test/add page data file (#10)

--- src/pages/[page].tsx
+++ src/pages/[page].tsx
@@ -6,6 +6,8 @@ import { getAllPages, getPageBySlug } from "@shared/services/page"
 
 import Content from "@scenes/Content"
 
+import fs from "fs"
+
 interface ContentProps {
   page: Page
 }
@@ -35,11 +37,21 @@ export const getStaticProps: GetStaticProps<
   }
 }
 
-export const getStaticPaths: GetStaticPaths = async () => {
+const getPagesFromSource = async (): Promise<Page[]> => {
+  const filePath = process.env["PAGE_DATA_FILE"]
+  if (filePath) {
+    return JSON.parse(fs.readFileSync(filePath, "utf-8")).pages as Page[]
+  }
+
   const pages_string = process.env["PAGE_DATA"]
-  const pages = pages_string
+
+  return pages_string
     ? (JSON.parse(pages_string).pages as Page[])
     : await getAllPages()
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const pages = await getPagesFromSource()
 
   const paths = pages.map((page) => {
     return { params: { page: page.slug } }
