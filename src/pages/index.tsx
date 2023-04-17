dac2b2fec1854a87cb36e9ccfd685a4509c15121
Ali Shouman <ali.shouman@yum.com>
2023-04-17 06:51:14 -0500

    Revert changes to only pass first 10 pages on home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,19 +8,27 @@ import Home from "@scenes/Home"
 
 interface HomeProps {
   pages: Page[]
+  showing: number
+  total: number
 }
 
-const HomePage: FC<HomeProps> = ({ pages }) => {
-  return <Home pages={pages} />
+const HomePage: FC<HomeProps> = ({ pages, showing, total }) => {
+  return <Home pages={pages} showing={showing} total={total} />
 }
 
 export default HomePage
 
 export const getStaticProps: GetStaticProps<HomeProps> = async () => {
   const pages = await getAllPages()
+  const toShow = pages.slice(0, 12)
+
+  const showing = toShow.length
+  const total = pages.length
   return {
     props: {
-      pages,
+      showing,
+      total,
+      pages: toShow,
     },
   }
 }
